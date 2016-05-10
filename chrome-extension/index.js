f05903cf8b4617a6de7e227acf685ebd60775a1e
Andreas Drivenes <andreas.drivenes@gmail.com>
2016-05-11 00:29:39 +0200

    Chrome  Ext: Inject content script programatically

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -3,6 +3,10 @@ ws = new WebSocket("ws://localhost:8080");
 ws.onmessage = function(e) {
     var data = JSON.parse(e.data);
 
+    chrome.tabs.executeScript({
+        file: 'popup.js'
+    });
+
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, data, function() {});
     });
