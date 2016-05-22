a584f6c2f0a8219d889eb97b3d62a82ccb078f86
Andreas Drivenes <andreas.drivenes@gmail.com>
2016-05-22 21:44:13 +0200

    Periodically check that the connection is up

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,14 +1,36 @@
-ws = new WebSocket("ws://localhost:8080");
+var ws;
 
-ws.onmessage = function(e) {
-    var data = JSON.parse(e.data);
 
-    chrome.tabs.executeScript({
-        file: 'popup.js'
-    });
+function openConnection() {
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, data, function() {});
-    });
-};
+    ws = new WebSocket("ws://localhost:8080");
 
+    ws.onmessage = function(e) {
+        var data = JSON.parse(e.data);
+
+        chrome.tabs.executeScript({
+            file: 'popup.js'
+        });
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, data, function() {});
+        });
+    };
+
+    ws.onclose = function() {
+        ws = undefined;
+    }
+
+    ws.onerror = function() {
+        ws = undefined;
+    }
+
+}
+
+chrome.alarms.create({when: Date.now() + 1000, periodInMinutes: 15});
+
+chrome.alarms.onAlarm.addListener(function() {
+    if (ws === undefined) {
+        openConnection();
+    }
+});
